270276621147cb7177b19c267e1f786b069341bd
Nicolasdavid0104 <142343386+Nicolasdavid0104@users.noreply.github.com>
2026-05-29 02:05:52 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,10 @@ import os
 import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from main import saudacao, calcular_media
 
-from main import autenticar_usuario
+def test_saudacao_nome_valido():
+    assert "Nicolas" in saudacao("Nicolas")
 
-def test_autenticacao_falsa():
-    # Nós sabemos que o usuário 'comum' deve retornar False
-    resultado = autenticar_usuario("comum")
-    
-    # Mas vamos forçar o teste a esperar True. 
-    # Isso vai fazer o Pytest estourar um X Vermelho na pipeline!
-    assert resultado is True
+def test_media_simples():
+    assert calcular_media([10, 8, 6]) == 8.0
